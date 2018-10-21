4c5e8ee857f46abe358ad417e555b70bb4fb4aeb
zengqiang <zengqiang724@163.com>
2018-10-21 22:10:23 +0800

    电力协议生成器

--- src/main/java/com/mytech/framework/protocol/generator/Protocol.java
+++
@@ -1,8 +0,0 @@
-package com.mytech.framework.protocol.generator;
-
-/**
- * Created by zengqiang on 2018/10/21.
- * 协议接口
- */
-public interface Protocol {
-}
--- src/main/java/com/mytech/framework/protocol/generator/ProtocolFactory.java
+++
@@ -1,8 +0,0 @@
-package com.mytech.framework.protocol.generator;
-
-/**
- * Created by zengqiang on 2018/10/21.
- * 协议工厂
- */
-public interface ProtocolFactory {
-}
