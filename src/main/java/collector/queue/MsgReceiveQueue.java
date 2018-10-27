54ea0e0b7ee93d255173953d4cbcebc89c915c99
zengqiang <zengqiang724@163.com>
2018-10-27 19:59:32 +0800

    发送消息队列跟接收消息队列

---
+++ src/main/java/collector/queue/MsgReceiveQueue.java
@@ -0,0 +1,8 @@
+package collector.queue;
+
+/**
+ * Created by zengqiang on 2018/10/27.
+ * 接收到的消息队列
+ */
+public class MsgReceiveQueue {
+}
---
+++ src/main/java/collector/queue/MsgSendQueue.java
@@ -0,0 +1,8 @@
+package collector.queue;
+
+/**
+ * Created by zengqiang on 2018/10/27.
+ * 要发送的消息队列
+ */
+public class MsgSendQueue {
+}
